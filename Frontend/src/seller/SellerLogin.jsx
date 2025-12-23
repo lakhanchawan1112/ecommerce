75d458333eb728664e92d5d369c6a289733892ec
lakhan chawan <lakhanchawan1112@gmail.com>
2025-12-23 18:15:11 +0530

    Seller Frontend

--- Frontend/src/seller/SellerLogin.jsx
+++ Frontend/src/seller/SellerLogin.jsx
@@ -1,6 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SellerLogin = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "", 
+    phone: "", 
+    company: "", 
+    password: "", 
+    confirmPassword:""
+  })
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name] : e.target.value
+    });
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("http://localhost:3000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+      });
+
+      const data = await res.json();
+      console.log(data);
+      alert(data.message);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <div>
         <form action="">
@@ -9,30 +46,30 @@ const SellerLogin = () => {
             <div className='seller-form'>
                 <div>
                 <label htmlFor="">Full Name</label>
-                <input type="text" placeholder='name'/>
+                <input name="name" placeholder='name'/>
                 </div>
                 <div>
                 <label htmlFor="">Email</label>
-                <input type="text" placeholder='email'/>
+                <input name="email" placeholder='email'/>
                 </div>
                 <div>
                 <label htmlFor="">Mobile No.</label>
-                <input type="text" placeholder='mobile no.'/>
+                <input name="phone" placeholder='mobile no.'/>
                 </div>
                 <div>
                 <label htmlFor="">Company Name</label>
-                <input type="text" placeholder='company name'/>
+                <input name="company" placeholder='company name'/>
                 </div>
                 <div>
                 <label htmlFor="">Password</label>
-                <input type="text" placeholder='password'/>
+                <input type="password" name='password' placeholder='password'/>
                 </div>
                 <div>
                 <label htmlFor="">Confirm Password</label>
-                <input type="text" placeholder='confirm password'/>
+                <input type="password" name="confirmPassword" placeholder='confirm password'/>
                 </div>
                 <div>
-                <button >Sign Up</button>
+                <button >Sign Up</button> 
                 </div>
             </div>
         </form>
